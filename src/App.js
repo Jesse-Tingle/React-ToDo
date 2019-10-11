5da8983b29a380a4acaacec46d124492ba52b8af
Jesse-Tingle <jtingle0@gmail.com>
2019-10-11 14:14:16 -0400

    set up state for to-do list

--- src/App.js
+++ src/App.js
@@ -3,14 +3,49 @@ import React from 'react';
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // this component is going to take care of state, 
+  // and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+
+    this.state = {
+      todos: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ],
+      todo: ''
+    };
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+
+        {/* Step 1: Display a list of To Dos */}
+
+
+        {/* Step 2: add an input field to add to-dos to the list */}
+
+        {/* Step 3: Create a submit button */}
+
+        {/* Step 4: Add a clear all button */}
+
+
+
       </div>
     );
   }
+
 }
 
 export default App;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,4 @@
+/*
+    `<TodoForm>` will hold your input field and 
+    your `Add Todo` and `Clear Completed` buttons.
+*/
